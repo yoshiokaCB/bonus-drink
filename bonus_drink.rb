6349b0cfeaafdbda6043320d259e8ab72ab50e4f
yoshioka <yoshioka@tm-21.com>
2018-01-02 23:43:42 +0900

    total_count_forメソッド修正

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
+  BONUS_COUNT = 3
   def self.total_count_for(amount)
-    0
+    total    = amount
+    decision = total
+    while decision >=3
+      bonus, remnant  = decision.divmod(BONUS_COUNT)
+      total          += bonus
+      decision        = bonus + remnant
+    end
+    total
   end
 end
